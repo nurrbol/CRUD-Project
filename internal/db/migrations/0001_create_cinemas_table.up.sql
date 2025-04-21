deb493c06cd5e24f4e19ee5c192689f7e3af5700
Nurda_Cyber <amirnurdaulet@gmail.com>
2025-04-22 04:15:35 +0500

    feat:migration

---
+++ internal/db/migrations/0001_create_cinemas_table.up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS cinemas (
+                                       id SERIAL PRIMARY KEY,
+                                       name TEXT NOT NULL,
+                                       location TEXT NOT NULL
+);
